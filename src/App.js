1235ebcce7af4d3adb1209d823caf4210262eb68
ramazankarisan <rkarisan25@gmail.com>
2021-12-27 23:57:11 +0100

    app created and commentList returned with axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
+import moment from 'moment'
 
 function App() {
+
+  const [commentList, setCommentList] = useState([]);
+
+
+  useEffect(() => {
+    axios.get(`https://react-yazi-yorum.herokuapp.com/posts`)
+      .then(response => setCommentList(response.data))
+  }, []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    // header
+    // postlist
+
+    <div className="main_wrapper">
+      <header>
+
       </header>
-    </div>
+
+      <div className="ui raised very padded text container segment">
+        <div className="ui relaxed divided list">
+
+          {commentList.map(item => {
+            return (<div className="item" key={item.id}>
+              <i className="large github middle aligned icon"></i>
+              <div className="content" >
+                <a className="header" > {item.title}</a >
+                <div className="description" > {moment(item.created_at).format('MMMM Do YYYY, h:mm:ss a')}</div >
+              </div >
+            </div >)
+          })}
+
+
+        </div >
+      </div >
+    </div >
+
+
+
   );
 }
 
